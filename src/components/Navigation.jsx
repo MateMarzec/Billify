de5598f6cf55f3ed236f46537620e00177054bfb
MateMarzec <64321779+MateMarzec@users.noreply.github.com>
2023-05-21 22:29:45 +0100

    Basic Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,36 @@
-function Navigation(){
-    return(
-        <>
-        
-        </>
-    )
+import { Link } from "react-router-dom";
+import { ChevronDown, Menu, ArrowRight } from "feather-icons-react/build/IconComponents";
+import Logo from "/logo.svg";
+import classes from "./Navigation.module.css";
+
+function Navigation() {
+  return (
+    <nav>
+      <div className={classes.container}>
+        <div className={classes.leftPanel}>
+          <Link to="/" className={classes.logo}>
+            <img src={Logo} alt="Billify Logo" />
+          </Link>
+          <Link to="/">
+            <p>Home</p>
+          </Link>
+          <div>
+            <p>Resources</p>
+            <ChevronDown />
+          </div>
+          <Link to="/">
+            <p>Contact</p>
+          </Link>
+        </div>
+        <div className={classes.rightPanel}>
+            <Link>
+                <button className="primary" type="button"> Generate Invoice <ArrowRight />
+                </button>
+            </Link>
+            <Menu />
+        </div>
+      </div>
+    </nav>
+  );
 }
 export default Navigation;
