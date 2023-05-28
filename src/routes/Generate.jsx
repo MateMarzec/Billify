c89de94a792563f833fb893cd90778c48b2a3a9f
MateMarzec <64321779+MateMarzec@users.noreply.github.com>
2023-05-28 17:30:58 +0100

    Generate boilerplate

--- src/routes/Generate.jsx
+++ src/routes/Generate.jsx
@@ -1,7 +1,20 @@
+import ContactUs from "../assets/payment.jpg";
+import classes from "./Generate.module.css";
+
 function Generate() {
   return (
-    <>
-    </>
+    <main className={classes.generateWrapper}>
+      <div className={classes.container}>
+        <section className={classes.leftSide}>
+          <div className={classes.content}>
+            <h1>Form</h1>
+          </div>
+        </section>
+        <div className={classes.rightSide}>
+          <div className={classes.preview}></div>
+        </div>
+      </div>
+    </main>
   );
 }
 
